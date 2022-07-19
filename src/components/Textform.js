29167da0341613cc889d2a0d027d154b2ed385b3
sahil kumar <sahil15kp@gmail.com>
2022-07-19 17:33:26 +1300

    space in buttons

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 export default function Textform(props) {
+
     const [Text, SetText] = useState('')// this is state, Text is default value that can be change by in the ('')
 
     const handleupclick = () => {
@@ -28,8 +29,8 @@ export default function Textform(props) {
 
                     <textarea className="form-control" id="textbox" onChange={handleonchange} style={{ backgroundColor: props.mode === 'dark' ? '#324978' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }} value={Text} rows="8"></textarea>
                 </div>
-                <button className='btn btn-success mx-2' onClick={handleupclick}>convert to uppercase</button>
-                <button className='btn btn-success mx-2' onClick={handleloclick}>convert to Lowercase</button>
+                <button  disabled={Text.length===0} className='btn btn-success mx-2 my-1' onClick={handleupclick} >convert to uppercase</button>
+                <button disabled={Text.length===0} className='btn btn-success mx-2 my-1' onClick={handleloclick}>convert to Lowercase</button>
             </div>
             <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>{/*we saying that if color:props.mode = dark then prit white otherwise excute the color code as given #042743*/}
                 <h1>Your  Text Summary</h1>
